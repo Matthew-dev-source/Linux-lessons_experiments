14d082529d9d4f0e3413f9aac50d5752f29960d9
Matthew-dev-source <cauamatthew@gmail.com>
2026-06-06 19:03:54 -0300

    add RAM notifier daemon

---
+++ RamNotifier.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true; do
+
+    total=$(free | grep Mem | awk '{print $2}')
+    used=$(free | grep Mem | awk '{print $3}')
+    percent=$(( used * 100 / total ))
+
+    if [ $percent -gt 15; ]; then
+        notify-send "Warning. RAM usage is at ${percent}"
+    fi
+
+    sleep 10
+done
