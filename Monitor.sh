8d1ab5cd30da9d307814ef4eb1ea1eb334e3b8dd
Matthew-dev-source <cauamatthew@gmail.com>
2026-06-07 20:23:45 -0300

    Ram and CPU monitor

---
+++ Monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pid=$(pgrep $1)
+
+if [ -n "$pid" ]; then
+    cpu=$(ps -p $pid -o %cpu,%mem | tail -n 1 | awk '{print $1}')
+    mem=$(ps -p $pid -o %cpu,%mem | tail -n 1 | awk '{print $2}')
+
+    echo "The process ID is $pid, CPU usage is at $cpu and memory is $mem"
+
+
+else
+
+    echo "This program is not running"
+
+fi
